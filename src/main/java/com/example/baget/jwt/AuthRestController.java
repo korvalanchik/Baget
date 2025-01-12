a408a982a223244f3621aa84d1c3e72487b29b05
korvalanchik <korvalan@yahoo.com>
2025-01-12 17:31:01 +0200

    Baget_2024 version 1.7.1 (Check role)

--- src/main/java/com/example/baget/jwt/AuthRestController.java
+++ src/main/java/com/example/baget/jwt/AuthRestController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.*;
 import java.security.Principal;
 import java.time.OffsetDateTime;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
