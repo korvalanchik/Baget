a6c55d65e5eecd98b0f214c3a99553da27be42e3
korvalanchik <korvalan@yahoo.com>
2025-08-24 21:16:20 +0300

    Baget_2024 version 2.3.22 (Transactions)

--- src/main/resources/db/migration/V22__create_transaction.sql
+++ src/main/resources/db/migration/V22__create_transaction.sql
@@ -4,16 +4,19 @@ CREATE TABLE transaction_types (
                                    description VARCHAR(100)          -- опис для користувача
 );
 
-
 CREATE TABLE transactions (
-                              transaction_id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                              order_no BIGINT NOT NULL,
-                              type_id BIGINT NOT NULL,
+                              transaction_id BIGINT AUTO_INCREMENT PRIMARY KEY, -- до кого належить транзакція
+                              customer_id BIGINT NULL, -- транзакція може бути пов’язана з конкретним замовленням (або NULL)
+                              order_no BIGINT NULL,
+                              type_id BIGINT NOT NULL,  -- Payment, Refund, Deposit, Withdrawal, Transfer
                               transaction_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
                               amount DECIMAL(10,2) NOT NULL,
-                              reference VARCHAR(50), -- номер рахунку, чеку і т.д.
-                              status VARCHAR(20),    -- Pending, Completed, Canceled
+                              reference VARCHAR(50), -- номер чеку, рахунку або внутрішній reference
+                              status VARCHAR(20) DEFAULT 'Pending', -- Pending, Completed, Canceled
                               note TEXT,
+
+                              CONSTRAINT fk_transaction_customer FOREIGN KEY (customer_id)
+                                  REFERENCES customer(CustNo),
 
                               CONSTRAINT fk_transaction_order FOREIGN KEY (order_no)
                                   REFERENCES orders(OrderNo),
@@ -21,3 +24,14 @@ CREATE TABLE transactions (
                               CONSTRAINT fk_transaction_type FOREIGN KEY (type_id)
                                   REFERENCES transaction_types(type_id)
 );
+
+INSERT INTO transaction_types (code, description) VALUES
+                                                      ('INVOICE', 'Рахунок'),
+                                                      ('PAYMENT', 'Оплата'),
+                                                      ('REFUND', 'Повернення'),
+                                                      ('ADJUSTMENT', 'Коригування'),
+                                                      ('TRANSFER', 'Переказ'),
+                                                      ('CHARGE', 'Нарахування'),
+                                                      ('DISCOUNT', 'Знижка'),
+                                                      ('CANCEL', 'Скасування'),
+                                                      ('ADVANCE_PAYMENT', 'Аванс');
