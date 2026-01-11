e86973e74ba85da42a5639ed015e568247163252
korvalanchik <korvalan@yahoo.com>
2026-01-11 13:08:19 +0200

    Baget_2024 version 2.7.15 (branches for manager)

--- src/main/java/com/example/baget/branch/BranchResource.java
+++ src/main/java/com/example/baget/branch/BranchResource.java
@@ -1,8 +1,12 @@
 package com.example.baget.branch;
 
+import com.example.baget.users.User;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/branch", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -15,5 +19,14 @@ public class BranchResource {
     public ResponseEntity<Boolean> existsBranch(@RequestParam("name") String branchName) {
         boolean exists = branchRepository.findByName(branchName).isPresent();
         return ResponseEntity.ok(exists);
+    }
+
+    @GetMapping("/manager")
+    public List<BranchDTO> getAllowedBranches(Authentication auth) {
+        User user = (User) auth.getPrincipal();
+
+        return user.getAllowedBranches().stream()
+                .map(b -> new BranchDTO(b.getBranchNo(), b.getName()))
+                .toList();
     }
 }
