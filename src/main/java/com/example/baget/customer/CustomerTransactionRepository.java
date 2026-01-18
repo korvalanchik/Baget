c5b5fb691d74d4b1070c0c9c5922a323f638b92b
korvalanchik <korvalan@yahoo.com>
2026-01-18 13:30:05 +0200

    Baget_2024 version 2.7.17.1 (Customer & finance transactions)

--- src/main/java/com/example/baget/customer/CustomerTransactionRepository.java
+++ src/main/java/com/example/baget/customer/CustomerTransactionRepository.java
@@ -1,11 +1,8 @@
 package com.example.baget.customer;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.lang.NonNull;
 
 import java.math.BigDecimal;
 import java.util.List;
--- src/main/java/com/example/baget/finance/FinanceTransactionRepository.java
+++ src/main/java/com/example/baget/finance/FinanceTransactionRepository.java
@@ -1,6 +1,5 @@
 package com.example.baget.finance;
 
-import com.example.baget.customer.CustomerTransaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FinanceTransactionRepository extends JpaRepository<FinanceTransaction, Long> {
