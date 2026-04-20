dd0de99d1c589819fe79572179644ad134d2d59c
korvalanchik <korvalan@yahoo.com>
2026-04-20 13:40:56 +0300

    Baget_2024 version 2.10.5 (Bug with DateTime)

--- src/main/java/com/example/baget/customer/CustomerDashboardDTO.java
+++ src/main/java/com/example/baget/customer/CustomerDashboardDTO.java
@@ -1,7 +1,7 @@
 package com.example.baget.customer;
 
 import java.math.BigDecimal;
-import java.time.OffsetDateTime;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class CustomerDashboardDTO {
@@ -25,7 +25,7 @@ public class CustomerDashboardDTO {
             String mobile,
             BigDecimal balance,
             Integer invoiceCount,
-            OffsetDateTime lastPaymentDate
+            LocalDateTime lastPaymentDate
     ) {}
 
     public record Payer(
